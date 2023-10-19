ce4b4c7cfa0ba76977a1dc9ae120ca5af5cdfc12
Linh Truong <hoailinhtruong12397@gmail.com>
2023-10-19 19:42:55 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { LoadingButton } from "@mui/lab";
+import "./App.css";
+import { useFetch } from "./hooks/useFetch";
+import { Box, Typography } from "@mui/material";
 
+const URL = "https://api.quotable.io/random";
 function App() {
+  const { data, loading, error } = useFetch(URL);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="flex-start"
+      maxWidth="800px"
+      margin="200px auto"
+      padding="20px"
+      border="2px solid #000"
+    >
+      {loading ? (
+        <>
+          <LoadingButton loading variant="text" sx={{ margin: "auto" }} />
+          <Typography variant="body1" sx={{ margin: "auto", fontSize: 30 }}>
+            Loading...
+          </Typography>
+        </>
+      ) : error ? (
+        <Typography variant="body1" sx={{ margin: "auto" }}>
+          Error: {error.message}
+        </Typography>
+      ) : (
+        <>
+          <Typography sx={{ fontSize: 30, fontWeight: 800, margin: "auto" }}>
+            Quote
+          </Typography>
+          <Typography sx={{ fontSize: 20 }}>
+            Content: {data && data.content}
+          </Typography>
+          <Typography sx={{ fontSize: 20 }}>
+            Author: {data && data.author}
+          </Typography>
+          <Typography sx={{ fontSize: 20 }}>
+            Tags: {data && data.tags}
+          </Typography>
+          <Typography sx={{ fontSize: 20 }}>
+            Author Slug: {data && data.authorSlug}
+          </Typography>
+          <Typography sx={{ fontSize: 20 }}>
+            Length: {data && data.length}
+          </Typography>
+        </>
+      )}
+    </Box>
   );
 }
 
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(response.statusText);
+        const data = await response.json();
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
